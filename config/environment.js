fe04e4bdfa6746149a715336ff3240e425ddab90
Andy Pickler <andy.pickler@gmail.com>
2015-09-15 11:58:27 -0600

    Install, configure, and deploy EmberFire

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'events',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://blistering-fire-1344.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
