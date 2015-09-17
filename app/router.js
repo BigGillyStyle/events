b914d4eff1401274a09e1395e3a0beece9e984a1
Andy Pickler <andy.pickler@gmail.com>
2015-09-17 17:58:17 -0600

    Changed routes, models, and pod structure around to support more standard RESTful routes

--- app/router.js
+++ app/router.js
@@ -5,8 +5,12 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
-  this.route('event');
+Router.map(function () {
+  this.route('event', {path: 'events'}, function () {
+    this.route('new');
+    this.route('show', {path: '/:event_id'});
+    this.route('edit', {path: '/:event_id/edit'});
+  });
 });
 
 export default Router;
