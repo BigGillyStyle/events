1b4e20911048f03440f4e25093444af352237a43
Andy Pickler <andy.pickler@gmail.com>
2015-09-19 16:18:13 -0600

    Corrected date/time disparity

--- app/pods/event/index/route.js
+++ app/pods/event/index/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.query('event', {
-      orderBy: 'date',
+      orderBy: 'time',
       startAt: new Date(new Date().setDate(new Date().getDate()-1)).getTime(),
       limitToFirst: 10
     });
--- app/pods/event/model.js
+++ app/pods/event/model.js
@@ -1,10 +1,23 @@
+import Ember from 'ember';
 import DS from 'ember-data';
+import momentFormat from 'ember-moment/computeds/format';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   city: DS.attr('string'),
   state: DS.attr('string', {defaultValue: 'AL'}),
   country: DS.attr('string', {defaultValue: 'US'}),
-  date: DS.attr('number'),
+  time: DS.attr('number'),
+  date: Ember.computed('time', {
+    get() {
+      return new Date(this.get('time'));
+    },
+    set(key, value) {
+      console.log("Setting date with key: " + key);
+      console.log("Setting date with value: " + value);
+      this.set('time', (new Date(value).getTime()));
+      return value;
+    }
+  }),
   url: DS.attr('string')
 });
