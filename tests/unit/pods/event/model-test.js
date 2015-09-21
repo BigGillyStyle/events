3c3f69c4bf9642e88327a73163f41ce7257f059f
Andy Pickler <andy.pickler@gmail.com>
2015-09-21 14:31:41 -0600

    Add some model tests

--- tests/unit/pods/event/model-test.js
+++ tests/unit/pods/event/model-test.js
@@ -1,12 +1,57 @@
-import { moduleForModel, test } from 'ember-qunit';
+import {
+  moduleForModel, test
+}
+from 'ember-qunit';
 
 moduleForModel('event', 'Unit | Model | event', {
   // Specify the other units that are required for this test.
   needs: []
 });
 
-test('it exists', function(assert) {
+test('it exists', function (assert) {
   let model = this.subject();
   // var store = this.store();
   assert.ok(!!model);
+});
+
+// attribute tests
+test('it has an attribute: name', function (assert) {
+  var model = this.subject();
+  var hasAttr = Object.keys(model.toJSON()).indexOf('name') > -1;
+  assert.ok(hasAttr);
+});
+
+test('it has an attribute: city', function (assert) {
+  var model = this.subject();
+  var hasAttr = Object.keys(model.toJSON()).indexOf('city') > -1;
+  assert.ok(hasAttr);
+});
+
+test('it has an attribute: state', function (assert) {
+  var model = this.subject();
+  var hasAttr = Object.keys(model.toJSON()).indexOf('state') > -1;
+  assert.ok(hasAttr);
+});
+
+test('it has an attribute: country', function (assert) {
+  var model = this.subject();
+  var hasAttr = Object.keys(model.toJSON()).indexOf('country') > -1;
+  assert.ok(hasAttr);
+});
+
+test('it has an attribute: time', function (assert) {
+  var model = this.subject();
+  var hasAttr = Object.keys(model.toJSON()).indexOf('time') > -1;
+  assert.ok(hasAttr);
+});
+
+test('it has an attribute: url', function (assert) {
+  var model = this.subject();
+  var hasAttr = Object.keys(model.toJSON()).indexOf('url') > -1;
+  assert.ok(hasAttr);
+});
+
+test('it defaults "state" to "AL"', function (assert) {
+  var model = this.subject();
+  assert.equal(model.get('state'), 'AL');
 });
