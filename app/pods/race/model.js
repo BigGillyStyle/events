8cf462dbc2c03c15be14f62de6c2d250844f76a8
Andy Pickler <andy.pickler@gmail.com>
2015-09-22 14:04:43 -0600

    Remove ember-validations

---
+++ app/pods/race/model.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  // Based on research on 9/22/2015...polymorphic models (STI) with Ember Data 2.0
+  // do not appear to be ready for primetime
+});
