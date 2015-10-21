3c4fc332bbf0cae7f5642f77cbc8c7f928e9e2c6
Andy Pickler <andy.pickler@gmail.com>
2015-10-21 15:18:20 -0600

    Revise button placement, refactor functionality

--- app/events/new/controller.js
+++ app/events/new/controller.js
@@ -3,13 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     submit() {
-        let model = this.store.createRecord('event', this.get('model'));
-        model.save().then((response) => {
-          this.transitionToRoute('events.show.races.new', response.id);
-        });
-      },
-    cancel() {
-      this.transitionToRoute('events.index');
+      let model = this.store.createRecord('event', this.get('model'));
+      model.save().then((response) => {
+        this.transitionToRoute('events.show.races.new', response.id);
+      });
     }
   }
 });
