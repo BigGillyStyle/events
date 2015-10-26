1f38d837811e4323327c3131f76c91ffa861ddb7
Andy Pickler <andy.pickler@gmail.com>
2015-10-26 09:10:15 -0600

    Add race model unit tests

---
+++ tests/unit/race/model-test.js
@@ -0,0 +1,35 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('race', 'Unit | Model | race', {
+  // Specify the other units that are required for this test.
+  needs: ['model:event', 'service:run-types', 'service:distance-converter']
+});
+
+test('it exists', function(assert) {
+  let model = this.subject();
+  // var store = this.store();
+  assert.ok(!!model);
+});
+
+test('should return a shortNameLabel', function(assert) {
+  let model = this.subject({
+    type: 'run',
+    shortName: 'marathon'
+  });
+  assert.equal(model.get('shortNameLabel'), 'Marathon');
+});
+
+test('should indicate that it isRun', function(assert) {
+  let model = this.subject({
+    type: 'run'
+  });
+  assert.equal(model.get('isRun'), true);
+});
+
+test('should convert bikeKilometers to bikeMiles', function(assert) {
+  const hundred = 100;
+  let model = this.subject({
+    bikeKilometers: hundred
+  });
+  assert.equal(model.get('bikeMiles'), hundred * 0.621371);
+});
